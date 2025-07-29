98835376e2611d2b749255c904a73defed7a299d
takataka06 <206020787+takataka06@users.noreply.github.com>
2025-07-29 15:22:02 +0000

    Add users edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+    @user = User.find(params[:id])
+  end
   def create
     @user = User.new(user_params)   
     if @user.save
@@ -20,6 +23,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
   private
 
   def user_params
