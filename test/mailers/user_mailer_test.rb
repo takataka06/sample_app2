d3b5de192687e26a71fcfdf3751df912bcb31c71
takataka06 <206020787+takataka06@users.noreply.github.com>
2025-08-01 07:36:37 +0000

    Add password reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -13,12 +13,16 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email),  mail.body.encoded # エスケープされたメールアドレスが含まれていることを確認
   end #正規表現と文字列の一致をmatchメソッドで確認
 
+
   test "password_reset" do
-    mail = UserMailer.password_reset
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal [user.email], mail.to
+    assert_equal ["user@realdomain.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 
 end
